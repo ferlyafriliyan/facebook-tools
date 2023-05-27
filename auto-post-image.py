35d703227a7335d98999ebbb615295316e181ea7
[ Dvanmeploph - ( Ferly Afriliyan - Itsmeafriliyan ) ] - <122451961+Itsmeafriliyan@users.noreply.github.com>
2023-05-27 19:18:09 +0700

    Update auto-post-image.py

--- auto-post-image.py
+++ auto-post-image.py
@@ -1,4 +1,4 @@
-# ------ [ Gw Cuma Perecode Biasa Bang!!! ] ------ #
+# ------ [ Denventa ] ------ #
 Author = 'Afriliyan Ferly Shishigami X'
 Version = 0.1
 Facebook = 'Facebook.com/AfriliyanFerly.Shishigami.X'
